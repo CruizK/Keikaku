9731144e6dc9458c355e910d1d5752908147df9e
CruizK <Cruiz.Kusman@gmail.com>
2020-01-01 21:28:18 -0600

    Deleted TextureAtlas, created TextureSheet

---
+++ Game/src/Models/TextureSheet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Keikaku.Models
+{
+    public class TextureSheet
+    {
+        List<Rectangle> rectangles;
+
+        public TextureSheet(Texture2D texture, int textureWidth, int textureHeight)
+        {
+            rectangles = new List<Rectangle>();
+
+            int cols = texture.Width / textureWidth;
+            int rows= texture.Height / textureHeight;
+
+            for(int y = 0; y < rows; y++)
+            {
+                for(int x = 0; x < cols; x++)
+                {
+                    rectangles.Add(new Rectangle(x * textureWidth, y * textureHeight, textureWidth, textureHeight));
+                }
+            }
+        }
+
+        public Rectangle getTextureRect(int index)
+        {
+            return rectangles[index];
+        }
+    }
+}
